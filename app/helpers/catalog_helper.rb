1b86cff8554fcc93829ab49f3602ff1d10594ffe
Daniel Pierce <dlpierce@indiana.edu>
2020-05-08 13:09:38 -0400

    Avoid unneeded solr lookup and add spec.

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -9,13 +9,19 @@ module CatalogHelper
   # @param [SolrDocument, Presenter] document
   # @return [String]
   def thumbnail_url document
-    representative_document = ::SolrDocument.find(document.thumbnail_id)
+    if document.id == document.thumbnail_id
+      representative_document = document
+    else
+      representative_document = ::SolrDocument.find(document.thumbnail_id)
+    end
+
     thumbnail_file_id = representative_document.original_file_id
     if thumbnail_file_id
       Hyrax.config.iiif_image_url_builder.call(thumbnail_file_id, nil, '250,')
     else
       raise 'thumbnail_file_id is nil'
     end
+
   rescue
     image_path 'default.png'
   end
