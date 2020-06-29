0df2b0e882a3795dd09bc0618f3f6807cc9876b9
Randall Floyd <rdfloyd@indiana.edu>
2020-06-29 13:08:08 -0400

    Initially adding the IIIF search annotations module in order to override

---
+++ app/models/blacklight_iiif_search/iiif_search_annotation.rb
@@ -0,0 +1,43 @@
+# corresponds to IIIF Annotation resource
+module BlacklightIiifSearch
+  class IiifSearchAnnotation
+    include IIIF::Presentation
+    include BlacklightIiifSearch::AnnotationBehavior
+
+    attr_reader :document, :query, :hl_index, :snippet, :controller,
+                :parent_document
+
+    ##
+    # @param [SolrDocument] document
+    # @param [String] query
+    # @param [Integer] hl_index
+    # @param [String] snippet
+    # @param [CatalogController] controller
+    # @param [SolrDocument] parent_document
+    def initialize(document, query, hl_index, snippet, controller, parent_document)
+      @document = document
+      @query = query
+      @hl_index = hl_index
+      @snippet = snippet
+      @controller = controller
+      @parent_document = parent_document
+    end
+
+    ##
+    # @return [IIIF::Presentation::Annotation]
+    def as_hash
+      annotation = IIIF::Presentation::Annotation.new('@id' => annotation_id)
+      annotation.resource = text_resource_for_annotation if snippet
+      annotation['on'] = canvas_uri_for_annotation
+      annotation
+    end
+
+    ##
+    # @return [IIIF::Presentation::Resource]
+    def text_resource_for_annotation
+      clean_snippet = ActionView::Base.full_sanitizer.sanitize(snippet)
+      IIIF::Presentation::Resource.new('@type' => 'cnt:ContentAsText',
+                                       'chars' => clean_snippet)
+    end
+  end
+end
