a0c7f630654961133f41229b8176dbe5f4f79275
Andy Smith <andjsmit@iupui.edu>
2020-07-29 08:56:17 -0400

    Optimize image used for OCR with textcleaner and bitonal conversions.

--- app/services/processors/ocr.rb
+++ app/services/processors/ocr.rb
@@ -9,7 +9,11 @@ module Processors
         Rails.logger.info "Copying Pre-derived OCR file #{existing_file} to #{output_file}."
         execute "cp #{existing_file} #{output_file}"
       else
-        execute "tesseract #{path} #{output_file.gsub('.xml', '')} #{options[:options]} alto"
+        clean_file = ocr_clean_file(path)
+        bitonal_file = ocr_bitonal_file(clean_file)
+        remove_tmp_file(clean_file)
+        execute "tesseract #{bitonal_file} #{output_file.gsub('.xml', '')} #{options[:options]} alto"
+        remove_tmp_file(bitonal_file)
       end
     end
 
@@ -29,6 +33,22 @@ module Processors
       return false unless File.exist?(ocr_file)
 
       ocr_file
+    end
+
+    def self.ocr_clean_file(path)
+      clean_file = File.join(Hydra::Derivatives.temp_file_base, "clean_#{File.basename(path)}")
+      execute "#{Rails.root.join('bin', 'textcleaner')} #{path} #{clean_file}"
+      clean_file
+    end
+
+    def self.ocr_bitonal_file(path)
+      bitonal_file = File.join(Hydra::Derivatives.temp_file_base, "bitonal_#{File.basename(path)}")
+      execute "convert #{path} -threshold 42% #{bitonal_file}"
+      bitonal_file
+    end
+
+    def self.remove_tmp_file(file)
+      execute "rm #{file}"
     end
 
     private
