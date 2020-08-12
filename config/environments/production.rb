94e09ebd6b755a95ff9372da993ef4bca0de73c4
Daniel Pierce <dlpierce@indiana.edu>
2020-08-12 12:50:44 -0400

    Increase reconnection attempts for ActionCable redis connection.
    This may workaround ECONNRESET issues that seem common with ActionCable.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,15 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  # Enable reconnect to redis to reduce disconnection issues with ActionCable PubSub
+  ActionCable::SubscriptionAdapter::Redis.redis_connector = lambda do |config|
+    redis_opts = { url: config[:url] }
+    redis_opts[:reconnect_attempts] = 5
+    redis_opts[:reconnect_delay] = 0.5
+    redis_opts[:reconnect_delay_max] = 5
+    ::Redis.new(redis_opts)
+  end
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
