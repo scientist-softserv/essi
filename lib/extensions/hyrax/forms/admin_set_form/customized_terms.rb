a43d81dfda83a593286eee381b8c931c671ad4d2
Adam Ploshay <aploshay@iu.edu>
2020-09-23 13:49:48 -0400

    [ESSI-1011] [ESSI-1073] Add university_place to AdminSet terms

--- lib/extensions/hyrax/forms/admin_set_form/customized_terms.rb
+++ lib/extensions/hyrax/forms/admin_set_form/customized_terms.rb
@@ -1,3 +1,5 @@
+# AdminSetForm in Hyrax presents multi-valued attributes as single-valued
+# These modifications work around that workaround, as needed
 module Extensions
   module Hyrax
     module Forms
@@ -5,19 +7,22 @@ module Extensions
         module CustomizedTerms
           def self.included(base)
             base.class_eval do
-              self.terms = [:title, :description, :thumbnail_id]
+              self.terms = [:title, :description, :thumbnail_id, :university_place]
+              class_attribute :allowed_array_terms
+              self.allowed_array_terms = [:university_place]
 
-              # Cast any array values on the model to scalars.
+              # Cast array values on the model to scalars, with exceptions
               def [](key)
                 return super if key == :thumbnail_id
+                return super if key.in? allowed_array_terms   
                 super.first
               end
 
               class << self
-                # This determines whether the allowed parameters are single or multiple.
-                # By default it delegates to the model.
-                def multiple?(_term)
-                  false
+                # Normally delegates to the model, but Hyrax overrides to return false
+                # Modified to allow exceptions to carry through as true
+                def multiple?(term)
+                  term.in? allowed_array_terms
                 end
               end
             end
