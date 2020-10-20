e386a4f3b0f4cfe16b1b5888071ef7bca3924935
Adam Ploshay <aploshay@iu.edu>
2020-10-20 11:17:19 -0400

    [ESSI-922] [ESSSI-1112] make files optional for archival material

--- app/forms/hyrax/archival_material_form.rb
+++ app/forms/hyrax/archival_material_form.rb
@@ -4,5 +4,9 @@ module Hyrax
   # Generated form for ArchivalMaterial
   class ArchivalMaterialForm < Hyrax::PagedResourceForm
     self.model_class = ::ArchivalMaterial
+
+    def work_requires_files?
+      false
+    end
   end
 end
--- spec/forms/hyrax/archival_material_form_spec.rb
+++ spec/forms/hyrax/archival_material_form_spec.rb
@@ -1,7 +1,14 @@
-# Generated via
-#  `rails generate hyrax:work ArchivalMaterial`
 require 'rails_helper'
 
-RSpec.describe Hyrax::ArchivalMaterialForm do
-  # "Add your tests here"
+RSpec.describe Hyrax::ArchivalMaterialForm, :clean do
+  let(:work) { FactoryBot.build(:archival_material) }
+  let(:ability) { Ability.new(FactoryBot.create(:user)) }
+  let(:repository) { double }
+  let(:form) { described_class.new(work, ability, repository) }
+
+  describe '#work_requires_files?' do
+    it 'returns false' do
+      expect(form.work_requires_files?).to eq false
+    end
+  end
 end
