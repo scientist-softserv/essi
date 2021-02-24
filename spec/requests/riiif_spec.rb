8282ca4a37b0008b8398358b47dc7720bfa03c87
Adam Ploshay <aploshay@iu.edu>
2021-02-24 11:45:26 -0500

    Gemfile.lock updates

--- spec/requests/riiif_spec.rb
+++ spec/requests/riiif_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'IIIF image API', type: :request do
     it 'should include http basic auth' do
       login_as user
 
-      expect(Riiif::HTTPFileResolver::RemoteFile).to receive(:new).with(
+      expect(Riiif::HttpFileResolver::RemoteFile).to receive(:new).with(
         anything, hash_including(basic_auth_credentials: auth_params )).and_call_original
 
       get Riiif::Engine.routes.url_helpers.image_path(file.id, size: 'max')
