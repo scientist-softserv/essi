4e028aa913581bd0944431c4a953f93bcdbbbfe7
Adam Ploshay <aploshay@iu.edu>
2021-05-27 12:59:30 -0400

    [ESSI-1336] [ESSI-1346] Add present? check to dynamic_schema_id CSV pull

--- lib/extensions/bulkrax/entry/dynamic_schema_field.rb
+++ lib/extensions/bulkrax/entry/dynamic_schema_field.rb
@@ -10,7 +10,7 @@ module Extensions
         end
 
         def add_dynamic_schema
-          return if self.parsed_metadata['dynamic_schema_id']
+          return if self.parsed_metadata['dynamic_schema_id'].present?
           context_id = find_metadata_context_for(admin_set_id: self.parsed_metadata['admin_set_id'],
                                                  profile_id: parser.parser_fields&.[]('profile_id'))
           self.parsed_metadata['dynamic_schema_id'] ||= ::AllinsonFlex::DynamicSchema.find_by(context_id: context_id, allinson_flex_class: factory_class.to_s)&.id
