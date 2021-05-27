c2fd93e9fddceae18a19ca737897836d81c2cd54
Adam Ploshay <aploshay@iu.edu>
2021-05-27 14:12:31 -0400

    [ESSI-1336] [ESSI-1350] Allow for CSV providing the profile version or id

--- lib/extensions/bulkrax/entry/dynamic_schema_field.rb
+++ lib/extensions/bulkrax/entry/dynamic_schema_field.rb
@@ -12,8 +12,12 @@ module Extensions
         def add_dynamic_schema
           return if self.parsed_metadata['dynamic_schema_id'].present?
           context_id = find_metadata_context_for(admin_set_id: self.parsed_metadata['admin_set_id'],
-                                                 profile_id: parser.parser_fields&.[]('profile_id'))
+                                                 profile_id: profile_id)
           self.parsed_metadata['dynamic_schema_id'] ||= ::AllinsonFlex::DynamicSchema.find_by(context_id: context_id, allinson_flex_class: factory_class.to_s)&.id
+        end
+
+        def profile_id
+          [self.parsed_metadata['profile_version'], self.parsed_metadata['profile_id'], parser.parser_fields&.[]('profile_id')].map(&:to_i).select(&:positive?).first
         end
 
         def find_metadata_context_for(admin_set_id:, profile_id:)
