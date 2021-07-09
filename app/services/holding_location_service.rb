85c024451d968e216a3682d7bf740990020cd5ca
nianma <nianma@indiana.edu>
2021-07-09 09:45:38 -0400

    replace iucat api url, and remove duplicated campus location in views

--- app/services/holding_location_service.rb
+++ app/services/holding_location_service.rb
@@ -1,6 +1,7 @@
 module HoldingLocationService
   mattr_accessor :authority
   self.authority = Qa::Authorities::IucatLibraries.new
+  #self.authority = Qa::Authorities::Local::FileBasedAuthority.new('holding_locations')
 
   def self.select_options
     select_all_options.sort
