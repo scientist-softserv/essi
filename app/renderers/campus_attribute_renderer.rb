a9353cff38bce54b9598be65b10c26acba4b40c6
Adam Ploshay <aploshay@iu.edu>
2021-07-23 08:22:38 -0400

    [ESSI-1337] [ESSI-1391] Change campus link to facet search results

--- app/renderers/campus_attribute_renderer.rb
+++ app/renderers/campus_attribute_renderer.rb
@@ -1,12 +1,16 @@
 class CampusAttributeRenderer < Hyrax::Renderers::AttributeRenderer
-  def initialize(value, options = {})
-    super(:campus, value, options)
+  def initialize(values, options = {})
+    super(:campus, values, options)
   end
 
   def value_html
     Array(values).map do |value|
       location_string(CampusService.find(value))
     end.join("")
+  end
+
+  def campus_search_path(campus)
+    CampusSearchHelper.path(campus)
   end
 
   private
@@ -18,8 +22,13 @@ class CampusAttributeRenderer < Hyrax::Renderers::AttributeRenderer
 
    def location_string(loc)
      return unless loc
-     content_tag(:a,
-                 loc.dig(:term),
-                 href: loc.dig(:url))
+     content_tag(:a, loc.dig(:term), href: campus_search_path(loc))
+   end
+
+   class CampusSearchHelper
+     include Rails.application.routes.url_helpers
+     def self.path(campus)
+       self.new.search_catalog_path(f: { campus_sim: [campus.dig(:id)] })
+     end
    end
 end
