0929763f445879277a66278cf16958226a1a14e8
Adam Ploshay <aploshay@iu.edu>
2021-08-25 18:48:43 -0400

    [ESSI-1370] Soften condition for AllinsonFlex::Profile before_destroy check

--- lib/extensions/allinson_flex/prepend_profile.rb
+++ lib/extensions/allinson_flex/prepend_profile.rb
@@ -3,10 +3,10 @@ module Extensions
   module AllinsonFlex
     module PrependProfile
       private
-        # unmodified from allinson_flex
+        # modified from allinson_flex: checks profile_version match
         def check_admin_set(admin_set_id)
           a = AdminSet.find(admin_set_id)
-          return a.members.count > 0
+          return a.members.select { |m| m.profile_version == profile_version }.count > 0
         rescue
           true
         end
