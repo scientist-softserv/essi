243d8aff52e9091f40810baade3809701f3c763d
Adam Ploshay <aploshay@iu.edu>
2021-08-31 17:15:08 -0400

    [ESSI-1424][ESSI-1436] Monkeypatch refactor of #set_removed_filesets, #update_filesets

--- lib/extensions/bulkrax/object_factory/remove_update_filesets.rb
+++ lib/extensions/bulkrax/object_factory/remove_update_filesets.rb
@@ -2,37 +2,40 @@ module Extensions
   module Bulkrax
     module ObjectFactory
       module RemoveUpdateFilesets
-        # unmodified from bulkrax FileFactory
+        # modifies an existing fileset, injecting an additional File into the first file
+        def modify_fileset(fileset:, original_name:, mime_type:, file_path:)
+          fileset.files.first.create_version
+          opts = {}
+          opts[:path] = fileset.files.first.id.split('/', 2).last
+          opts[:original_name] = original_name
+          opts[:mime_type] = mime_type
+
+          fileset.add_file(::File.open(file_path), opts)
+          fileset.save
+          ::CreateDerivativesJob.set(wait: 1.minute).perform_later(fileset, fileset.files.first.id)
+          nil
+        end
+
+        # modified from bulkrax: uses helper method
         def set_removed_filesets
           local_file_sets.each do |fileset|
-            fileset.files.first.create_version
-            opts = {}
-            opts[:path] = fileset.files.first.id.split('/', 2).last
-            opts[:original_name] = 'removed.png'
-            opts[:mime_type] = 'image/png'
-    
-            fileset.add_file(::File.open(::Bulkrax.removed_image_path), opts)
-            fileset.save
-            ::CreateDerivativesJob.set(wait: 1.minute).perform_later(fileset, fileset.files.first.id)
+            modify_fileset(fileset: fileset,
+                           original_name: 'removed.png',
+                           mime_type: 'image/png',
+                           file_path: ::Bulkrax.removed_image_path)
           end
         end
 
-        # unmodified from bulkrax FileFactory
+        # modified from bulkrax: uses helper method
         def update_filesets(current_file)
           if @update_files && local_file_sets.present?
             fileset = local_file_sets.shift
             return nil if fileset.files.first.checksum.value == Digest::SHA1.file(current_file.file.path).to_s
-    
-            fileset.files.first.create_version
-            opts = {}
-            opts[:path] = fileset.files.first.id.split('/', 2).last
-            opts[:original_name] = current_file.file.file.original_filename
-            opts[:mime_type] = current_file.file.content_type
-    
-            fileset.add_file(File.open(current_file.file.to_s), opts)
-            fileset.save
-            ::CreateDerivativesJob.set(wait: 1.minute).perform_later(fileset, fileset.files.first.id)
-            nil
+   
+            modify_fileset(fileset: fileset,
+                           original_name: current_file.file.file.original_filename,
+                           content_type: current_file.file.content_type,
+                           file_path: current_file.file.to_s) 
           else
             current_file.save
             current_file.id
