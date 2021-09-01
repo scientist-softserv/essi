eb96517ec52255de003cfb8949b4823d875d3093
Daniel Pierce <dlpierce@indiana.edu>
2021-09-01 16:31:27 -0400

    Fix sequential jobs when using inline/test adapter

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -10,12 +10,12 @@ class ApplicationJob < ActiveJob::Base
   end
   
   after_perform do |job|
-    Thread.current[:essi_nested_job_parent_id] = nil # Does this fail when perform_now is used?
     # actually enqueue jobs
     while nested_job = Thread.current[:essi_nested_job_queue].shift do
       Rails.logger.debug { "Enqueuing #{nested_job.inspect}. #{Thread.current[:essi_nested_job_queue].size} jobs left to queue." }
       nested_job.enqueue
     end
+    Thread.current[:essi_nested_job_parent_id] = nil
   end
   
   # store jobs to be enqueued after current job is complete
@@ -25,7 +25,7 @@ class ApplicationJob < ActiveJob::Base
       super(*args)
     else
       # nested_job_service.push(new(*args))
-      Rails.logger.debug { "Thread storing #{self.class} with args #{args.inspect} with #{Thread.current[:essi_nested_job_queue].size} jobs in the queue." }
+      Rails.logger.debug { "Thread storing #{self.name} with args #{args.inspect} with #{Thread.current[:essi_nested_job_queue].size} jobs in the queue." }
       Thread.current[:essi_nested_job_queue].push(new(*args))
     end
   end
