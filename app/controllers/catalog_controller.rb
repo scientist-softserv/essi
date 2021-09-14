8953c2d979df1295f8b4c6503e09dd2590c9121f
Adam Ploshay <aploshay@iu.edu>
2021-09-14 11:29:01 -0400

    [ESSI-1321][ESSI-1446] prep changes for dynamic catalog behavior

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,7 +1,7 @@
 class CatalogController < ApplicationController
   include Hydra::Catalog
   include Hydra::Controller::ControllerBehavior
-  include AllinsonFlex::DynamicCatalogBehavior
+  include ESSI::DynamicCatalogBehavior
   # This filter applies the hydra access controls
   before_action :enforce_show_permissions, only: :show
 
