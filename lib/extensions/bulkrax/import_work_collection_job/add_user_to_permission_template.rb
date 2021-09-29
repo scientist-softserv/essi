c5e09bc52c70b94dab0f864bef4a9b03573c9ab2
Adam Ploshay <aploshay@iu.edu>
2021-09-29 10:37:59 -0400

    [ESSI-1424][ESSI-1461] Monkeypatch bulkrax import job to allow existing collection access

--- lib/extensions/bulkrax/import_work_collection_job/add_user_to_permission_template.rb
+++ lib/extensions/bulkrax/import_work_collection_job/add_user_to_permission_template.rb
@@ -4,13 +4,13 @@ module Extensions
       module AddUserToPermissionTemplate
         private
 
-        # unmodified from bulkrax
+        # modified from bulkrax to allow existing PermissionTemplateAccess
         def add_user_to_permission_template!(entry)
           user                = ::User.find(entry.importerexporter.user_id)
           collection          = entry.factory.find
           permission_template = ::Hyrax::PermissionTemplate.find_or_create_by!(source_id: collection.id)
     
-          ::Hyrax::PermissionTemplateAccess.create!(
+          ::Hyrax::PermissionTemplateAccess.find_or_create_by!(
             permission_template_id: permission_template.id,
             agent_id: user.user_key,
             agent_type: 'user',
