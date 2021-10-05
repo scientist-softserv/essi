999a9c90dfea6a43c447cafe753e3f6dc064b6e6
Daniel Pierce <dlpierce@indiana.edu>
2021-10-05 11:59:19 -0400

    Move sequential job queueing to module so bulkrax can use it

--- app/jobs/bulkrax/application_job.rb
+++ app/jobs/bulkrax/application_job.rb
@@ -1,5 +1,7 @@
 # Job classes based on ApplicationJob will wait to enqueue any nested jobs until
 # the current job has finished.
-class ApplicationJob < ActiveJob::Base
-  prepend SequentialJob
+module Bulkrax
+  class ApplicationJob < ActiveJob::Base
+    prepend SequentialJob
+  end
 end
