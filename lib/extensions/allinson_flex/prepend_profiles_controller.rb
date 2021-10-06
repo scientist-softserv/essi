9b2c76912115124bd74c20da46ba80d682d59e58
Adam Ploshay <aploshay@iu.edu>
2021-10-06 14:02:59 -0400

    [ESSI-1465] Catch RecordInvalid in ProfilesController#import

--- lib/extensions/allinson_flex/prepend_profiles_controller.rb
+++ lib/extensions/allinson_flex/prepend_profiles_controller.rb
@@ -9,7 +9,7 @@ module Extensions
           redirect_to profiles_path, alert: 'Please select a file to upload'
           return
         end
- 
+
         begin 
           @allinson_flex_profile = ::AllinsonFlex::Importer.load_profile_from_path(path: uploaded_io.path)
         rescue ::AllinsonFlex::Importer::YamlSyntaxError => e
@@ -18,22 +18,22 @@ module Extensions
         rescue ::AllinsonFlex::Validator::InvalidDataError
           default_schema = ::JSONSchemer.schema(::Pathname.new(::AllinsonFlex.m3_schema_path))
           data = ::YAML.load_file(uploaded_io.path)
-          @validation_error = 'Profile data failed schema validation.  <a href="/profiles/log">See logs for full details of all errors.</a>  Initial errors are:<br/><br/>'
           errors = default_schema.validate(data).to_a
-          errors.each do |error|
-            %w[type details data_pointer data schema_pointer schema].each do |key|
-              @validation_error += "#{key.titleize}: #{error[key]}<br/>" if error[key].present?
-            end
-            @validation_error += "<br/>"
+          details = errors.map do |error|
+            %w[type details data_pointer data schema_pointer schema].map do |key|
+              "#{key.titleize}: #{error[key]}<br/>" if error[key].present?
+            end.join
           end
+          set_validation_error(action: 'schema validation', details: details)
+        rescue ::ActiveRecord::RecordInvalid => e
+          set_validation_error(action: 'construction', details: e.to_s.split(', '))
         end
  
-        if @validation_error.present?
-          redirect_to profiles_path, alert: @validation_error.to_s.truncate(800)
-        elsif @allinson_flex_profile.save
+        if @validation_error.blank? && @allinson_flex_profile.save
           redirect_to profiles_path, notice: 'AllinsonFlexProfile was successfully created.'
         else
-          redirect_to profiles_path, alert: @allinson_flex_profile.errors.messages.to_s.truncate(800)
+          set_validation_error(action: 'save', details: @allinson_flex_profile.errors.full_messages) if @validation_error.blank?
+          redirect_to profiles_path, alert: @validation_error.to_s.truncate(800)
         end
       end
 
@@ -42,6 +42,13 @@ module Extensions
         add_breadcrumbs
         log_path = ESSI.config.dig(:essi, :metadata, :profile_log)
         @logs = File.exists?(log_path) ? File.read(log_path) : 'No logs available'
+      end
+
+      private
+
+      def set_validation_error(action:, details:)
+        @validation_error = "Profile data failed #{action}.  <a href=\"/profiles/log\">See logs for full details of all errors.</a>  Initial errors are:<br/><br/>"
+        @validation_error += details.join("<br/>")
       end
     end
   end
