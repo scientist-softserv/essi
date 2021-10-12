f624194880342d5032538a97ae103a7c39218fe7
Adam Ploshay <aploshay@iu.edu>
2021-10-12 16:34:31 -0400

    [ESSI-1471] Add configuration for geonames

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -119,7 +119,7 @@ Hyrax.config do |config|
 
   # Location autocomplete uses geonames to search for named regions
   # Username for connecting to geonames
-  # config.geonames_username = ''
+  config.geonames_username = ESSI.config.dig(:geonames, :username)
 
   # Should the acceptance of the licence agreement be active (checkbox), or
   # implied when the save button is pressed? Set to true for active
