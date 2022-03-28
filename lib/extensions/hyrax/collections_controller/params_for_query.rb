87c9d19a3c2e80f2de5461f47df7990627766705
Randall Floyd <rdfloyd@indiana.edu>
2022-03-28 17:34:59 -0400

    Overriding query params so that collection searches use all_fields

---
+++ lib/extensions/hyrax/collections_controller/params_for_query.rb
@@ -0,0 +1,17 @@
+module Extensions
+  module Hyrax
+    module CollectionsController
+      module ParamsForQuery
+        private
+
+        # You can override this method if you need to provide additional inputs to the search
+        # builder. For example:
+        #   search_field: 'all_fields'
+        # @return <Hash> the inputs required for the collection member query service
+        def params_for_query
+          params.merge(search_field: 'all_fields', q: params[:cq])
+        end
+      end
+    end
+  end
+end
