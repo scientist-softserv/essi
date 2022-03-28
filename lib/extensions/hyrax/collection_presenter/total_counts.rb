77c1bc6b16ba9a8e93c2137bc6e42a938581d406
Adam Ploshay <aploshay@iu.edu>
2022-03-28 16:51:36 -0400

    [ESSI-1566] monkeypatch collection totals methods to total tree counts

--- lib/extensions/hyrax/collection_presenter/total_counts.rb
+++ lib/extensions/hyrax/collection_presenter/total_counts.rb
@@ -3,20 +3,30 @@ module Extensions
   module Hyrax
     module CollectionPresenter
       module TotalCounts
+        delegate :num_collections, :num_works, to: :solr_document
+
+        def tree_collection_ids
+          @tree_collection_ids ||= ::Collection.tree_collections_for(id).map(&:id)
+        end
+
+        def all_items
+          ::ActiveFedora::Base.where(member_of_collection_ids_ssim: tree_collection_ids)
+        end
+
         def total_items
-          ::ActiveFedora::Base.where("member_of_collection_ids_ssim:#{id}").count
+          all_items.count
         end
     
         def total_viewable_items
-          ::ActiveFedora::Base.where("member_of_collection_ids_ssim:#{id}").accessible_by(current_ability).count
+          all_items.accessible_by(current_ability).count
         end
     
         def total_viewable_works
-          ::ActiveFedora::Base.where("member_of_collection_ids_ssim:#{id} AND generic_type_sim:Work").accessible_by(current_ability).count
+          all_items.where(generic_type_sim: 'Work').accessible_by(current_ability).count
         end
     
         def total_viewable_collections
-          ::ActiveFedora::Base.where("member_of_collection_ids_ssim:#{id} AND generic_type_sim:Collection").accessible_by(current_ability).count
+          all_items.where(generic_type_sim: 'Collection').accessible_by(current_ability).count
         end
       end
     end
