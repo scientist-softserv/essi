1f750ff155531401853ea7c70fbda7aab9a68a74
Adam Ploshay <aploshay@iu.edu>
2022-08-05 12:24:06 -0400

    [ESSI-1645] more SelectCollectionListPresenter specs

--- spec/presenters/hyrax/select_collection_list_presenter.rb
+++ spec/presenters/hyrax/select_collection_list_presenter.rb
@@ -1,14 +1,39 @@
-RSpec.describe Hyrax::SelectCollectionTypeListPresenter, :clean_repo do
+RSpec.describe Hyrax::SelectCollectionTypeListPresenter do
   let(:user) { create(:user) }
   let(:ability) { ::Ability.new(user) }
   let(:instance) { described_class.new(ability) }
   let(:authorized_collection) { create(:collection_lw, title: ['Authorized Collection'], user: user) }
+  let(:authorized_collection2) { create(:collection_lw, title: ['Authorized Collection 2'], user: user) }
   let(:forbidden_collection) { create(:collection_lw, title: ['Forbidden Collection']) }
 
-  describe '#options_for_select' do
+  describe "#many?" do
+    context "without 0/1 authorized collections" do
+      expect(instance.many?).to eq false
+    end
+    context "without multiple authorized collections", :clean do
+      authorized_collection
+      authorized_collection2
+      expect(instance.many?).to be true
+    end
+  end
+
+  describe "#any?" do
+    context "without any authorized collections" do
+      forbidden_collection
+      expect(instance.any?).to eq true
+    end
+    context "with authorized collections", :clean do
+      authorized_collection
+      expect(instance.many?).to be true
+    end
+  end
+
+  describe '#options_for_select', :clean do
     let(:subject) { subject.options_for_select.map(&:reverse).to_h }
 
     it 'returns a sorted array of authorized collection titles, ids (as Strings)' do
+      authorized_collection
+      forbidden_collection
       expect(subject[authorized_collection.id]).to eq authorized_collection.to_s
       expect(subject[forbidden_collection.id]).to be_nil
     end
