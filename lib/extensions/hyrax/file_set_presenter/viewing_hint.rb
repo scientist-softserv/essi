7bb4ad83bd2bcac74e7168ea6082caf8b19d0117
Adam Ploshay <aploshay@iu.edu>
2022-11-30 11:08:35 -0500

    [ESSI-1658] remove redundant delegation call

--- lib/extensions/hyrax/file_set_presenter/viewing_hint.rb
+++ lib/extensions/hyrax/file_set_presenter/viewing_hint.rb
@@ -3,8 +3,6 @@ module Extensions
     module FileSetPresenter
       module ViewingHint
         delegate :viewing_hint, :thumbnail_id, to: :solr_document
-        #@todo remove after upgrade to Hyrax 3.x
-        delegate :original_file_id, to: :solr_document
       end
     end
   end
