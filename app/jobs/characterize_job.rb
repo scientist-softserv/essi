5233ba7220340d9de6aeed5449153882ea4fb654
Adam Ploshay <aploshay@iu.edu>
2023-01-06 11:35:46 -0500

    [ESSI-1765] CharacterizeJob unlinks characterization file, parent dir

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -6,9 +6,12 @@ class CharacterizeJob < ApplicationJob
   # @param [FileSet] file_set
   # @param [String] file_id identifier for a Hydra::PCDM::File
   # @param [String, NilClass] filepath the cached file within the Hyrax.config.working_path
-  def perform(file_set, file_id, filepath = nil, derivation_path = nil)
+  def perform(file_set, file_id, filepath = nil, derivation_path = nil, delete_characterization_path = false)
     raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
-    filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id) unless filepath && File.exist?(filepath)
+    unless filepath && File.exist?(filepath)
+      filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id) unless filepath && File.exist?(filepath)
+      delete_characterization_path = false
+    end
     Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filepath)
     Rails.logger.debug "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
     file_set.characterization_proxy.save!
@@ -16,6 +19,15 @@ class CharacterizeJob < ApplicationJob
     file_set.parent&.in_collections&.each(&:update_index)
     derivation_path = filepath unless derivation_path && File.exist?(derivation_path)
     CreateDerivativesJob.perform_later(file_set, file_id, derivation_path)
+    unlink_filepath(filepath, delete_characterization_path) if delete_characterization_path
+  end
+
+  # Removes characterization file and optionally parent directory
+  # @param [String] filepath file to unlink
+  # @param [Boolean, String] options option to remove parent directory
+  def unlink_filepath(filepath, options)
+    File.unlink(filepath)
+    Dir.rmdir(File.dirname(filepath)) if options == 'include_parent_dir'
   end
 end
 
