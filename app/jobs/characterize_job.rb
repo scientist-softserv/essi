9ed6c3244f991ab61547c467b539bc7850b3d7aa
Adam Ploshay <aploshay@iu.edu>
2023-01-06 15:50:37 -0500

    [ESSI-1765] add documentation for new arguments

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -6,6 +6,7 @@ class CharacterizeJob < ApplicationJob
   # @param [FileSet] file_set
   # @param [String] file_id identifier for a Hydra::PCDM::File
   # @param [String, NilClass] filepath the cached file within the Hyrax.config.working_path
+  # @param [FalseClass, TrueClass, String, NilClass] delete_characterization_path triggers deleting the file on any truthy value, and the parent directory on 'include_parent_dir'
   def perform(file_set, file_id, filepath = nil, derivation_path = nil, delete_characterization_path = false)
     raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
     unless filepath && File.exist?(filepath)
@@ -19,13 +20,13 @@ class CharacterizeJob < ApplicationJob
     file_set.parent&.in_collections&.each(&:update_index)
     derivation_path = filepath unless derivation_path && File.exist?(derivation_path)
     CreateDerivativesJob.perform_later(file_set, file_id, derivation_path)
-    unlink_filepath(filepath, delete_characterization_path) if delete_characterization_path
+    unlink_filepath(filepath, delete_characterization_path.to_s) if delete_characterization_path
   end
 
   # Removes characterization file and optionally parent directory
   # @param [String] filepath file to unlink
-  # @param [Boolean, String] options option to remove parent directory
-  def unlink_filepath(filepath, options)
+  # @param [String, NilClass] options option to remove parent directory
+  def unlink_filepath(filepath, options = nil)
     File.unlink(filepath)
     Dir.rmdir(File.dirname(filepath)) if options == 'include_parent_dir'
   end
