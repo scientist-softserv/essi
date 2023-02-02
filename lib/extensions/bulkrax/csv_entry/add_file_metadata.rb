ab5bc3e81aedaf2e0a180d64561ae736ac0db28f
Adam Ploshay <aploshay@iu.edu>
2023-02-02 00:27:04 -0500

    [ESSI-875] patch Bulkrax::CsvEntry to parse file metadata

--- lib/extensions/bulkrax/csv_entry/add_file_metadata.rb
+++ lib/extensions/bulkrax/csv_entry/add_file_metadata.rb
@@ -1,9 +1,25 @@
-# unmodified from bulkrax
+# modified from bulkrax
 module Extensions
   module Bulkrax
     module CsvEntry
       module AddFileMetadata
-       # unmodified from bulkrax
+        # modified to add file metadata
+        def add_file
+          super
+          add_file_metadata
+        end
+
+        def add_file_metadata
+          case record['file_label']
+          when String
+            labels = record['file_label'].split(/\s*[;|]\s*/)
+          when Array
+            labels = record['file_label']
+          else
+            labels = []
+          end
+          self.parsed_metadata['file_metadata'] = labels.map { |label| { label: label } }
+        end
       end
     end
   end
